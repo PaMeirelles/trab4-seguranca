3b177956f71b857f367788894c5dc2d52106af0d
Rafael <rafaelpmeirelles@gmail.com>
2024-05-08 12:33:31 -0300

    User registration skeleton

--- src/DatabaseManager.java
+++ src/DatabaseManager.java
@@ -1,8 +1,23 @@
+import java.security.PrivateKey;
+import java.security.cert.Certificate;
+
 public class DatabaseManager {
     public static boolean isFirstAccess(){
         return true;
     }
     public static boolean loginIsUnique(String login){
         return true;
+    }
+
+    private static String preparePassword(String password){
+        return "";
+    }
+
+    private static byte[] preparePrivateKey(PrivateKey privateKey){
+        return new byte[0];
+    }
+
+    public static void saveUser(String login, String password, PrivateKey privateKey, Certificate certificate, String friendlyName){
+
     }
 }
